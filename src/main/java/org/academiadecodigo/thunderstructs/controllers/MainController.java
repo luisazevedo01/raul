d2044c518e947775a0fdae83e7cd2a9596993fb7
Filipe António <filipe.aa1995@gmail.com>
2019-12-08 15:09:35 -0100

    added maincontroller and view

--- src/main/java/org/academiadecodigo/thunderstructs/controllers/MainController.java
+++ src/main/java/org/academiadecodigo/thunderstructs/controllers/MainController.java
@@ -1,14 +1,15 @@
 package org.academiadecodigo.thunderstructs.controllers;
 
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-public class HomeController {
+public class MainController {
 
-    @RequestMapping(method = RequestMethod.GET , value = {"/", ""})
-    public String show(){
-        return "index";
+    @RequestMapping(method = RequestMethod.GET, value = {"","/"})
+    public String showMainMenu(){
+        return "MainMenu";
     }
 }
