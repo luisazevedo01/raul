179ddee6e3b71288c8c2ec970ab6126264c58398
João Furnas <joaofurnas@gmail.com>
2019-12-08 17:32:00 -0100

    created register service, controller and view

--- src/main/java/org/academiadecodigo/thunderstructs/controllers/HomeController.java
+++ src/main/java/org/academiadecodigo/thunderstructs/controllers/HomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class HomeController {
 
-    @RequestMapping(method = RequestMethod.GET , value = {"/", ""})
+    @RequestMapping(method = RequestMethod.GET , value = "/")
     public String show(){
         return "index";
     }
